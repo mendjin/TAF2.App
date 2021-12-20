40ed38f3808f06f276ec859b6f2bc65ebac64a99
mendjin michelle <mendjinmichelle@gmail.com>
2021-12-20 11:51:05 +0100

    Ajoutez des fichiers projet.

---
+++ TAF2/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TAF2
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigationView_Loaded(object sender ,RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(Page1));
+        }
+
+        private void NavigationView_SelectionChanged (NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+          if(args.IsSettingsSelected)
+            {
+
+            }
+          else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+                switch(item.Tag.ToString())
+                {
+                    case "Page1":
+                        ContentFrame.Navigate(typeof(Page1));
+                        break;
+                    case "Page2":
+                        ContentFrame.Navigate(typeof(Page2));
+                        break;
+                }
+            }
+        }
+
+    }
+}
